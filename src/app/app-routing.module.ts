a72274193bf2a4b74f929f9dd24fbf04e2eddbc8
Raúl Llanos <rau.llanos@duocuc.cl>
2023-09-24 00:53:22 -0300

    Agregando auth-screens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './auth-screens/auth-screen/sign-in/sign-in.component';
+import { AuthScreenComponent } from './auth-screens/auth-screen/auth-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: '',
+  loadChildren: ()=>import('./public/public.module').then((m)=>m.PublicModule),
+},
+{
+  path: 'auth-screen',
+  component: AuthScreenComponent,
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
